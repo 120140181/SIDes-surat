93e3c395acfa868fbad3ac7cf00a06337a2e8b72
psychomachy999 <fatkhanas03@gmail.com>
2025-11-11 09:18:22 +0700

    UI Enhancement

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') - Sistem Surat Desa</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo.svg') }}">
 
     <!-- AdminLTE -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
